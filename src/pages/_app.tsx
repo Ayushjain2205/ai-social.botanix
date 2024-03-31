5a5c7ae645114ff4d6af30d0a36d221b6af8bb7b
Ayush Jain <ayushjain2205@gmail.com>
2024-04-01 00:09:53 +0530

    Update factory address

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,16 +6,17 @@ import {
   smartWallet,
 } from "@thirdweb-dev/react";
 import { Toaster } from "react-hot-toast";
+import { BotanixTestnet } from "@thirdweb-dev/chains";
 
 const smartWalletConfig = {
-  factoryAddress: "0x453B18Cc5d45962c6337A720C471B485c92dAc6F",
+  factoryAddress: "0x219c60335e728b195b2101b3A8f2c23570173fAC",
   gasless: true,
 };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      activeChain="binance-testnet"
+      activeChain={BotanixTestnet}
       clientId="aa131b3a51f3247c974b4611ac4ff73e"
       supportedWallets={[smartWallet(embeddedWallet(), smartWalletConfig)]}
     >
